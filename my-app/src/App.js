7593849429c62f43a650ea98e5a55e6160e8d165
Sabrina Fleita <sabrina.fleita13@gmail.com>
2022-12-08 13:58:32 -0300

    todo

---
+++ my-app/src/App.js
@@ -0,0 +1,82 @@
+import './App.css';
+
+import { Formulario } from './components/formulario';
+import { useState } from 'react';
+import { Tarea } from './components/Tarea'
+
+
+function App() {
+
+  const [tarea, setTarea] = useState('')
+  const [listadoTareas, setListadoTareas] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if(tarea === ''){
+      alert('Debes poner una tarea')
+      return
+    }
+    const nuevaTarea = {
+      id: Date.now(),
+      tarea: tarea,
+      completado: false
+    }
+    const temp = [nuevaTarea, ...listadoTareas]
+    setListadoTareas(temp)
+
+    setTarea('')
+   
+  }
+ 
+  function handleChange(e) {
+    setTarea(e.target.value)
+  
+  }
+  //function onActualizarTarea(objEditarTarea){
+    //const {id, tarea} = objEditarTarea
+
+    //const temp = [...listadoTareas]
+     //const elemento = temp.find(item => item.id === id)
+     //elemento.tarea = tarea
+
+     //setListadoTareas(temp)
+  //}
+  function onBorrarTarea(id){
+    const temp = listadoTareas.filter(item => item.id !== id)
+    setListadoTareas(temp)
+  }
+  return (
+    <>
+    <div className='contenedorPrincipal'>
+      <h1>To do List</h1>
+      <div className='contenedorFormulario'>
+        <Formulario 
+        tarea={tarea}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+        />
+      </div>
+      <div className='contenedorTareas'>
+       <h2>Lista de Tareas</h2>
+       <div className='contenedorInfoTareas'>
+       {
+        listadoTareas.map(tarea => (
+          <Tarea
+          key={tarea.id}
+          id={tarea.id}
+          tarea={tarea} 
+          //onActualizarTarea={onActualizarTarea}
+          onBorrarTarea={onBorrarTarea}
+          />
+        ))
+       }
+       </div>
+      </div>
+    </div>
+ 
+
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/formulario.js
@@ -0,0 +1,16 @@
+export function Formulario(props) {
+
+    const {tarea, handleSubmit, handleChange} = props
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Introduce la tarea"
+            onChange={handleChange}
+            value={tarea} />
+            <input type="submit"
+            className="btn"
+            value="AGREGAR"
+            onClick={handleSubmit} />
+        </form>
+    )
+}
